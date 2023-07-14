82e4141f532c5ca0658a0b670ace0b9d3be65020
Pukkalla <ashok.kumar.pukkalla@accenture.com>
2023-07-14 06:05:13 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-        import React from "react";
-        import ReactDOM from "react-dom";
+        import React, { createElement } from "react";
+        import ReactDOM from "react-dom/client";
         
-        const heading1 = React.createElement("h2",{},"Namaste From React!");
-        const heading2 = React.createElement("h2",{},"Namaste From React!");
-        const root1 = React.createElement("div",
-        {
-            id : "root1"
-        }
-        ,[heading1,heading2]);
+        const heading = React.createElement("h1",{id:"header"},"Namaste React🚀");
         const Root = ReactDOM.createRoot(document.getElementById("root"));
-        Root.render(root1);
+        /*we can see the difference clearly between line 4,5 with 7 &
+         if we write more than 1 line code we should put them in "()"*/
+        const jsxheading= (<h1 id="header" className="class">
+            Namaste React using JSX🚀
+            </h1>);
+        //React Functional components
+        const HeadingComponent = () => {
+            return <h1>Functional component using return</h1>
+        } 
+        const HeadingComponent2 = () => <h1>Functional component without return</h1>
+    
+        //also this is the component composition  
+        const HeadingComponent3 = () => (
+            <React.Fragment>
+                <div id="container">
+                    {jsxheading}
+                    <h1 className="comp">
+                        Functional component without return, in multiple lines
+                    </h1>
+                </div>
+                <div id="container2">
+                    <p>
+                        Functional component : It is a normal JS function that can return the JSX code or React element. 
+                    </p>
+                </div>
+            </React.Fragment>
+        )
+
+        Root.render(<HeadingComponent3/>);
